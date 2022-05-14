399a98f40cfa17ef1d5cedecf2d4e18ea9e8b89b
lethomas1975 <le.thomas@outlook.com>
2022-05-14 20:05:44 +0800

    adding eeprom

---
+++ Eeprom.X/eeprom.c
@@ -0,0 +1,13 @@
+/*
+ * File:   eeprom.c
+ * Author: thomas
+ *
+ * Created on 14 May 2022, 20:02
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ eLock.X/eeprom.c
@@ -0,0 +1,11 @@
+/*
+ * File:   eeprom.c
+ * Author: thomas
+ *
+ * Created on 14 May 2022, 20:03
+ */
+
+
+#include <xc.h>
+#include "../eLock.X/eeprom.h"
+
