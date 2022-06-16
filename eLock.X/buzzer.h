29010b6e8dc8aaee882862ee0ed7db7e74684dcf
Thomas Le <le.thomas@outlook.com>
2022-06-16 21:54:03 +0800

    update projects

--- eLock.X/buzzer.h
+++ eLock.X/buzzer.h
@@ -1,11 +1,3 @@
-#ifndef XC_HEADER_TEMPLATE_H
-#define	XC_HEADER_TEMPLATE_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <xc.h> // include processor files - each processor file is guarded.  
-#include <pic18f4550.h>
-#endif	/* XC_HEADER_TEMPLATE_H */
-
 #ifndef BUZZER_H
 #define BUZZER_H
 void buzz(void);
